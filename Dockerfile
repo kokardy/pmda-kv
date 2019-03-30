1058932131ed1c25871a3532780995c9dc714c30
kokardy <mgc1028@gmail.com>
2019-03-30 22:56:32 +0900

    http server

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 from alpine:3.9
 
-RUN apk --update add wget grep bash httpie && rm -rf /var/cache/apk/*
+RUN apk --update add wget grep bash httpie supervisor && rm -rf /var/cache/apk/*
 RUN mkdir -p /asset/data
 COPY bin /asset/bin
+COPY http /http
 COPY daily /var/spool/cron/crontabs/daily
+COPY supervisord.conf /etc/supervisord.conf
 RUN cd /asset/bin && ./Init.sh /asset/data
-CMD crond -l 2 -f
+CMD supervisord -c /etc/supervisord.conf
---
+++ http/server.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func pmdaPath(yjcode string) (path string, err error) {
+	f := fmt.Sprintf("/asset/bin/data/yj/%s", yjcode)
+	if _, err = os.Stat(f); err != nil {
+		return
+	}
+	r, err := os.OpenFile(f, os.O_RDONLY, 0666)
+	if err != nil {
+		return
+	}
+	buf := bytes.NewBuffer([]byte{})
+	_, err = io.Copy(buf, r)
+	if err != nil {
+		return
+	}
+	line := buf.String()
+	path = strings.Trim(line, "\r\n ")
+	return
+}
+
+func redirectToPMDA(c *gin.Context) {
+	yjcode := c.Param("yjcode")
+	path, err := pmdaPath(yjcode)
+	if err != nil {
+		message := fmt.Sprintf("NOT FOUND: %s \n error:%s", yjcode, err)
+		c.String(http.StatusNotFound, message)
+		return
+	}
+	var url = fmt.Sprintf("//pmda.go.jp/PmdaSearch/iyakuSearch/%s", path)
+
+	c.Redirect(http.StatusMovedPermanently, url)
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Converter(YJcode to PMDA path) works.")
+	})
+
+	//redirect to PMDA
+	r.GET("/redirect/:yjcode/", redirectToPMDA)
+
+	fmt.Println("listen: 80")
+	r.Run(":80")
+}
