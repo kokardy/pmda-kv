16d432e44f8e37a8ae1b0e5360705aaebeeaffe5
kokardy <mgc1028@gmail.com>
2020-02-05 01:11:06 +0900

    crontab 追加

--- bin/POSTsearchDay.sh
+++ bin/POSTsearchDay.sh
@@ -7,7 +7,7 @@ filename=$2
 echo saveDir $saveDir
 echo updateDocDt $updateDocDt
 #入力値チェック
-if [ -z $saveDir -o -z $updateDocDt ] ; then
+if [ "$saveDir" = ""  -o "$updateDocDt" = "" ] ; then
 	echo "num of command option must be 2."
 	echo "POSTsearchDay.sh [saveDir] [update_date(YYYYMMDD)]"
 	exit
