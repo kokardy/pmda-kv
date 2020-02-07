76806b4c5cb5deac0433e1c44d611eda02942390
kokardy <mgc1028@gmail.com>
2020-02-07 17:42:29 +0900

    crontab

--- bin/Init.sh
+++ bin/Init.sh
@@ -3,4 +3,4 @@ d=$1
 ./DLsearchresult.sh $d
 ./DLhtml.sh $d/searchresult/00000000_
 
-"0 2 * * * bash /asset/bin/Routine.sh /asset/data" | crontab -
+echo "15 2,6,17 * * * bash /asset/bin/Routine.sh /asset/data" | crontab -
