fd6ab6d58ec6478196463f8929e55ac5307d767b
Kota Kokado <kouta-k@mbm.nifty.com>
2020-02-19 18:13:35 +0900

    cronのログを/asset/cron.logに

--- bin/Init.sh
+++ bin/Init.sh
@@ -3,4 +3,4 @@ d=$1
 ./DLsearchresult.sh $d
 ./DLhtml.sh $d/searchresult/00000000_
 
-echo "15 2,6,17 * * * cd /asset/bin && bash Routine.sh /asset/data" | crontab -
+echo "15 2,6,17 * * * cd /asset/bin && bash Routine.sh /asset/data >> /asset/cron.log" | crontab -
