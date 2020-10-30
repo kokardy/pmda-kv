ad92856bf167db571fa471fe12dba25e4eb930fd
Kota Kokado <kouta-k@mbm.nifty.com>
2020-10-30 18:23:54 +0900

    プロウペスが6桁目がアルファベットだった

--- bin/extractYJ.sh
+++ bin/extractYJ.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 input=$1
-grep -o -e "[0-9]\{6\}[0-9A-Z][A-Z][0-9]\{4\}" $input | while read YJ; do
+grep -o -e "[0-9]\{5\}[0-9A-Z][0-9A-Z][A-Z][0-9]\{4\}" $input | while read YJ; do
 	filename=`basename $input`
 	value=${filename%.*}
 	./StoreKV.sh $YJ $value
